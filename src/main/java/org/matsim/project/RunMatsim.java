e344ae9468da2fe1403f9f4aa824a7c25927eb2c
WIGGI1995 <66122107+WIGGI1995@users.noreply.github.com>
2024-02-07 14:36:34 +0100

    Emissions with Noise offline calculation

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -1,21 +1,3 @@
-/* *********************************************************************** *
- * project: org.matsim.*												   *
- *                                                                         *
- * *********************************************************************** *
- *                                                                         *
- * copyright       : (C) 2008 by the members listed in the COPYING,        *
- *                   LICENSE and WARRANTY file.                            *
- * email           : info at matsim dot org                                *
- *                                                                         *
- * *********************************************************************** *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *   See also COPYING, LICENSE and WARRANTY file                           *
- *                                                                         *
- * *********************************************************************** */
 package org.matsim.project;
 
 import com.google.inject.internal.asm.$Type;
@@ -25,82 +7,131 @@ import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
-import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
-import org.matsim.core.config.groups.QSimConfigGroup;
-import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
-import org.matsim.core.config.groups.QSimConfigGroup.TrafficDynamics;
-import org.matsim.core.config.groups.QSimConfigGroup.VehiclesSource;
-import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
-import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.collections.CollectionUtils;
-import org.matsim.vehicles.VehicleType;
-import org.matsim.vehicles.VehicleUtils;
-import ch.sbb.matsim.mobsim.qsim.SBBTransitModule;
-import ch.sbb.matsim.mobsim.qsim.pt.SBBTransitEngineQSimModule;
-import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import org.matsim.contrib.emissions.EmissionModule;
 import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
 import org.matsim.core.controler.AbstractModule;
+import org.matsim.contrib.emissions.analysis.FastEmissionGridAnalyzer;
+import org.matsim.contrib.emissions.analysis.EmissionsOnLinkEventHandler;
+import org.matsim.contrib.emissions.analysis.EmissionsByPollutant;
+import org.matsim.api.core.v01.network.Network;
+import java.util.Map;
+import org.matsim.contrib.emissions.Pollutant;
+import org.matsim.contrib.emissions.analysis.Raster;
+import java.util.Locale;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.matsim.contrib.analysis.time.TimeBinMap;
+import org.matsim.contrib.noise.NoiseConfigGroup;
+import org.matsim.contrib.noise.NoiseOfflineCalculation;
+import org.matsim.contrib.noise.MergeNoiseCSVFile;
+import org.matsim.contrib.noise.ProcessNoiseImmissions;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Set;
+public class RunMatsim {
 
-/**
- * @author nagel
- *
- */
-public class RunMatsim{
+    public static void main(String[] args) {
+        Config config;
+        if (args == null || args.length == 0 || args[0] == null) {
+            config = ConfigUtils.loadConfig("C:\\models\\scenario-sim-6\\config.xml", new EmissionsConfigGroup(), new NoiseConfigGroup());
+        } else {
+            config = ConfigUtils.loadConfig(args, new EmissionsConfigGroup(), new NoiseConfigGroup());
+        }
 
-	public static void main(String[] args) {
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        Controler controler = new Controler(scenario);
 
-		Config config;
-		if ( args==null || args.length==0 || args[0]==null ){
-			config = ConfigUtils.loadConfig( "C:\\models\\scenario-sim-6\\config.xml", new EmissionsConfigGroup() );
-		} else {
-			config = ConfigUtils.loadConfig( args, new EmissionsConfigGroup() );
-		}
+        controler.addOverridingModule(new AbstractModule() {
+            @Override
+            public void install() {
+                bind(EmissionModule.class).asEagerSingleton();
+            }
+        });
 
-		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
+        EmissionsOnLinkEventHandler emissionsHandler = new EmissionsOnLinkEventHandler(config.qsim().getTimeStepSize());
+        controler.getEvents().addHandler(emissionsHandler);
 
-		// possibly modify config here
+        controler.run();
 
-		// ---
-		
-		Scenario scenario = ScenarioUtils.loadScenario(config) ;
+        performEmissionAnalysis(config, scenario);
+        outputEmissionsData(emissionsHandler, config);
 
-		// possibly modify scenario here
-		
-		// ---
-		
-		Controler controler = new Controler( scenario ) ;
-		
-		// possibly modify controler here
-		// controler.addOverridingModule( new OTFVisLiveModule() ) ;
-		// To use the deterministic pt simulation (Part 1 of 2):
-		//controler.addOverridingModule(new SBBTransitModule());
-		controler.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				bind(EmissionModule.class).asEagerSingleton();
-			}
-		});
-		// To use the fast pt router (Part 1 of 1)
-		//controler.addOverridingModule(new SwissRailRaptorModule());
+        performOfflineNoiseCalculation(config, scenario);
+    }
 
-		// To use the deterministic pt simulation (Part 2 of 2):
-		//controler.configureQSimComponents(components -> {
-		//			new SBBTransitEngineQSimModule().configure(components);
-		//		});
-		
-		// ---
-		
-		controler.run();
-	}
-	
+    private static void performEmissionAnalysis(Config config, Scenario scenario) {
+        String outputDirectory = config.controler().getOutputDirectory();
+        String eventsFile = outputDirectory + "/output_events.xml.gz";
+        String outputFile = outputDirectory + "/Emission_Grid_Analysis.csv";
+        
+        Network network = scenario.getNetwork();
+        Map<Pollutant, Raster> analysisResults = FastEmissionGridAnalyzer.processEventsFile(eventsFile, network, 50, 30);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            writer.write("Pollutant,CellX,CellY,Value\n");
+            analysisResults.forEach((pollutant, raster) -> {
+                raster.forEachCoordinate((x, y, value) -> {
+                    try {
+                        writer.write(pollutant + "," + x + "," + y + "," + value + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void outputEmissionsData(EmissionsOnLinkEventHandler emissionsHandler, Config config) {
+        String outputDirectory = config.controler().getOutputDirectory();
+        String emissionsFile = outputDirectory + "/link_emissions.csv";
+
+        try (FileWriter writer = new FileWriter(emissionsFile)) {
+            writer.write("timeBin;linkId;pollutant;maxEmission\n");
+            TimeBinMap<Map<Id<Link>, EmissionsByPollutant>> timeBins = emissionsHandler.getTimeBins();
+            for (TimeBinMap.TimeBin<Map<Id<Link>, EmissionsByPollutant>> timeBin : timeBins.getTimeBins()) {
+                double timeBinStart = timeBin.getStartTime();
+                Map<Id<Link>, EmissionsByPollutant> emissionsByLink = timeBin.getValue();
+                for (Map.Entry<Id<Link>, EmissionsByPollutant> entry : emissionsByLink.entrySet()) {
+                    Id<Link> linkId = entry.getKey();
+                    EmissionsByPollutant emissions = entry.getValue();
+                    for (Map.Entry<Pollutant, Double> pollutantEntry : emissions.getEmissions().entrySet()) {
+                        Pollutant pollutant = pollutantEntry.getKey();
+                        double maxEmission = pollutantEntry.getValue();
+                        String line = String.format(Locale.US, "%.2f;%s;%s;%.2f\n", timeBinStart, linkId, pollutant, maxEmission);
+                        writer.write(line);
+                    }
+                }
+            }
+            System.out.println("Emissions data saved to " + emissionsFile);
+        } catch (IOException e) {
+            System.err.println("Error writing emissions data to file: " + e.getMessage());
+        }
+    }
+
+    private static void performOfflineNoiseCalculation(Config config, Scenario scenario) {
+        String outputDirectory = config.controler().getOutputDirectory();
+        NoiseConfigGroup noiseParameters = ConfigUtils.addOrGetModule(config, NoiseConfigGroup.class);
+        NoiseOfflineCalculation noiseCalculation = new NoiseOfflineCalculation(scenario, outputDirectory);
+        noiseCalculation.run();
+
+        String outputFilePath = outputDirectory + "noise-analysis/";
+        ProcessNoiseImmissions process = new ProcessNoiseImmissions(outputFilePath + "immissions/", outputFilePath + "receiverPoints/receiverPoints.csv", noiseParameters.getReceiverPointGap());
+        process.run();
+
+        final String[] labels = { "immission", "consideredAgentUnits" , "damages_receiverPoint" };
+        final String[] workingDirectories = { outputFilePath + "immissions/", outputFilePath + "consideredAgentUnits/", outputFilePath + "damages_receiverPoint/" };
+
+        MergeNoiseCSVFile merger = new MergeNoiseCSVFile();
+        merger.setReceiverPointsFile(outputFilePath + "receiverPoints/receiverPoints.csv");
+        merger.setOutputDirectory(outputFilePath);
+        merger.setTimeBinSize(noiseParameters.getTimeBinSizeNoiseComputation());
+        merger.setWorkingDirectory(workingDirectories);
+        merger.setLabel(labels);
+        merger.run();
+    }
 }
